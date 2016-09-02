e1c6e758079b4229d80208bc4aa4f17a2b99551d
Luis Guillermo Parra <luis@luiss-MacBook-Pro-2.local>
2016-09-02 14:15:48 -0700

    arreglado

--- server.js
+++ server.js
@@ -7,11 +7,14 @@ const PORT = 8080;
 
 // App
 const app = express();
-require('dns').lookup(require('os').hostname(), function (err, add, fam) {
-  console.log('addr: '+add);
-})
+
 
 app.get('/', function (req, res) {
+
+	require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+  console.log('addr: '+add);
+})
+	
   res.send('Hello world, this is a new build number 2!!!!!!!\nfrom server : '+add);
 });
 
